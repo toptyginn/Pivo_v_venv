3c19e8719491a0f7ba731a8a490fd5b576473886
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2025-11-14 15:35:31 +0300

    fixed with database

--- document_cache_app/permissions_manager.py
+++ document_cache_app/permissions_manager.py
@@ -1,4 +1,4 @@
-from document_cache_app.database import fake_permissions_db
+from document_cache_app.database import fake_permissions_db, remove_permission, add_permission
 
 class PermissionsManager:
     def __init__(self):
@@ -9,13 +9,18 @@ class PermissionsManager:
             raise ValueError("user_id and permission must be provided")
         if document_id is None:
             document_id  = max(self.user_permissions.keys(), default=1)+1
-        fake_permissions_db.update({document_id: {"user_id": user_id, "permission_level": permission}})
+        try:
+            add_permission(document_id, user_id, permission)
+            self.user_permissions[document_id] = fake_permissions_db[document_id]
+        except ValueError:
+            raise KeyError("Failed to set permission for the given user and document ID")
         return self.user_permissions
 
     def revoke_permission(self, user_id, permission, document_id) -> dict[int, dict[list]]:
-        if document_id in fake_permissions_db:
+        if document_id in self.user_permissions:
             try:
-                fake_permissions_db[document_id].remove({"user_id": user_id, "permission_level": permission})
+                remove_permission(document_id, user_id)
+                self.user_permissions[document_id] = fake_permissions_db[document_id]
             except ValueError:
                 raise KeyError("Permission not found for the given user and document ID")
         else:
@@ -24,11 +29,11 @@ class PermissionsManager:
         
 
     def has_permission(self, user_id, permission, document_id) -> bool:
-        return fake_permissions_db.get(document_id) == {"user_id": user_id, "permission_level": permission}
+        return self.user_permissions.get(document_id) == {"user_id": user_id, "permission_level": permission}
 
     def list_permissions(self, user_id) -> list[dict[int, str]]:
         res = []
-        for doc_id, perm in fake_permissions_db.items():
+        for doc_id, perm in self.user_permissions.items():
             if perm["user_id"] == user_id:
                 res.append({doc_id: perm["permission_level"]})
         return res
